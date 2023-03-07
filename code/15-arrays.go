b7c36fd2fa0725d16511192763299b6ce8596a09
Nilanjan Bhattacharjee <nilanjan.bhattacharjee77@gmail.com>
2023-03-07 22:22:03 +0530

    added arrays code

---
+++ code/15-arrays.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var names [5]string
+	var numbers [3]int = [3]int{1, 2, 3}
+	fmt.Println(names)
+	fmt.Println(numbers)
+	fmt.Println(len(numbers))
+	fmt.Println(numbers[1])
+	fmt.Println("====================================")
+	names = [5]string{"a", "b", "c", "d", "e"}
+
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[i])
+	}
+	fmt.Println("====================================")
+
+	for index, element := range names {
+		fmt.Println(index, "=>", element)
+	}
+	fmt.Println("====================================")
+
+	mat := [2][2]int{{1, 2}, {3, 4}}
+
+	for i := 0; i < len(mat); i++ {
+		for j := 0; j < len(mat[i]); j++ {
+			fmt.Print(mat[i][j], "\t")
+		}
+		fmt.Println()
+	}
+	fmt.Println("====================================")
+}
