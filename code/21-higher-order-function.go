e48bb6be4bb4014ed9639bb2ba855a73f9d24721
Nilanjan Bhattacharjee <nilanjan.bhattacharjee77@gmail.com>
2023-03-08 23:38:30 +0530

    added higher order function

---
+++ code/21-higher-order-function.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+var PI float64 = 3.14
+
+func getArea(r float64) float64 {
+	return PI * r * r
+}
+
+func getPerimeter(r float64) float64 {
+	return 2 * PI * r
+}
+
+func higherOrder(r float64, calc func(r float64) float64) {
+	fmt.Println(calc(r))
+}
+func main() {
+	action := map[int]func(r float64) float64{
+		1: getArea,
+		2: getPerimeter,
+	}
+
+	var input int
+	var r float64
+	fmt.Println("enter option and radious carefully")
+	fmt.Scanf("%d %f", &input, &r)
+
+	fmt.Println(input, r)
+
+	higherOrder(r, action[input])
+
+}
